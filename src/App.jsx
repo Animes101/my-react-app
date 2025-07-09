488a9655fa4f64522ba7a8cc1a8fe85bc8d9d0a8
Animes <animesbarman101@gmail.com>
2025-07-09 17:15:11 +0600

    style react

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+const date=new Date();
+const year=date.getFullYear();
+const month=date.getMonth();
+
+const h4Style={
+  color:'green',
+  textAlign:'center'
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    <div className='react'>
+      <h1>Hello react</h1>
+      <p>{year}</p>
+      <p>{month}</p>
+
+
+
+      <h3 style={{color:'red',fontSize:'23px',textAlign:'center'}}>wellcome to react</h3>
+
+      <h4 style={h4Style}>internal style</h4>
+    </div>
+      
+        
     </>
   )
 }
