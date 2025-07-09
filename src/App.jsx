2e83ea3733e66ac310031979db7771a6425169fb
Animes <animesbarman101@gmail.com>
2025-07-09 18:16:40 +0600

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 
 import './App.css'
 
-const date=new Date();
-const year=date.getFullYear();
-const month=date.getMonth();
+// const date=new Date();
+// const year=date.getFullYear();
+// const month=date.getMonth();
 
-const h4Style={
-  color:'green',
-  textAlign:'center'
-}
+// const h4Style={
+//   color:'green',
+//   textAlign:'center'
+// }
+
+
+import Card from './components/Card'
 
 function App() {
   
@@ -16,7 +19,7 @@ function App() {
   return (
     <>
 
-    <div className='react'>
+    {/* <div className='react'>
       <h1>Hello react</h1>
       <p>{year}</p>
       <p>{month}</p>
@@ -26,8 +29,20 @@ function App() {
       <h3 style={{color:'red',fontSize:'23px',textAlign:'center'}}>wellcome to react</h3>
 
       <h4 style={h4Style}>internal style</h4>
-    </div>
-      
+    </div> */}
+
+    {/* <div className='Card'>
+      <h1>Card Title</h1>
+      <p>card Body Lorem ipsum dolor sit amet consectetur adipisicing elit. Est dolor facilis nostrum 
+        impedit facere similique sint quis odio voluptatem eligendi?</p>
+        <button>View</button>
+    </div> */}
+
+    {/* Components React */}
+     <Card />
+     <Card />
+     <Card />
+     <Card />
         
     </>
   )
---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './card.css'
+
+export default function Card() {
+  return (
+    <div className='card'>
+        <h1>Card Title </h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, hic.</p>
+        <button>View Details</button>
+    </div>
+  )
+}
